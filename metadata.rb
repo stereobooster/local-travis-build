971e5a8f25519a3c8b9f6e16188c7666dc4cc590
stereobooster <stereobooster@gmail.com>
2015-12-21 01:46:36 +0300

    First try with berkshelf + chef

---
+++ metadata.rb
@@ -0,0 +1,10 @@
+name             'myapp'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures myapp'
+long_description 'Installs/Configures myapp'
+version          '0.1.0'
+
+depends 'ruby_rbenv', '~> 1.0.1'
+depends 'ruby_build', '~> 0.8.0'
